a447464db17382bb7e04143ac0d5302bd3a4abc5
salma <salmaemam52@gmail.com>
2023-08-22 23:16:27 +0300

    storess migration added

--- database/migrations/2023_08_22_201114_create_stores_table.php
+++ database/migrations/2023_08_22_201114_create_stores_table.php
@@ -11,13 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('tags', function (Blueprint $table) {
+        Schema::create('stores', function (Blueprint $table) {
             $table->id();
-            $table->string('name')->unique();
             $table->timestamps();
         });
-
-  
     }
 
     /**
@@ -25,7 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('tags');
-
+        Schema::dropIfExists('stores');
     }
 };
